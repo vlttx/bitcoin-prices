f72a33bad8c13f542b2be44710300ad5bb670c47
VBTX <jv.svedaite@gmail.com>
2019-11-17 10:33:09 -0600

    add bootstrap theme from bootswatch and apply to layout and navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,15 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 const Layout = props => (
   <div>
-    {" "}
+    <Head>
+      <title>Bitcoin prices</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+      />
+    </Head>
     <Navbar />
     {props.children}
   </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,40 +2,27 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-      </ul>
-      <style jsx>
-        {`
-          ul {
-            background: #333;
-            color: #fff;
-            list-style: none;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-evenly;
-          }
-          ul li {
-            font-size: 18px;
-            margin-right: 20px;
-          }
-          a {
-            color: #fff;
-            text-decoration: none;
-          }
-        `}
-      </style>
-    </div>
+    <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Bitcoin Price
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link href="/">
+                <a className="nav-link">Home</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about">
+                <a className="nav-link">About</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
