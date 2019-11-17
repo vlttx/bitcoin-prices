8dc9ad86e433eba1f88320aa892e24bf83766ae5
VBTX <jv.svedaite@gmail.com>
2019-11-17 10:02:29 -0600

    add css style for navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,40 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+      <style jsx>
+        {`
+          ul {
+            background: #333;
+            color: #fff;
+            list-style: none;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-evenly;
+          }
+          ul li {
+            font-size: 18px;
+            margin-right: 20px;
+          }
+          a {
+            color: #fff;
+            text-decoration: none;
+          }
+        `}
+      </style>
+    </div>
   );
 };
 
